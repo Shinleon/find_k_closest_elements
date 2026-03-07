5186fb313df301c772be763cd0bdf7394fc81f61
leonard shin <leonardshin1@gmail.com>
2026-03-06 16:12:18 -0800

    remove build directory

--- cmake_example/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/leonardshin/Documents/c_prog/find_k_closest_elements/cmake_example
-# Build directory: /Users/leonardshin/Documents/c_prog/find_k_closest_elements/cmake_example/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/leonardshin/Documents/c_prog/find_k_closest_elements/cmake_example/build/hello_test[1]_include.cmake")
-subdirs("_deps/googletest-build")
--- cmake_example/build/hello_test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/Users/leonardshin/Documents/c_prog/find_k_closest_elements/cmake_example/build/hello_test[1]_tests.cmake")
-  include("/Users/leonardshin/Documents/c_prog/find_k_closest_elements/cmake_example/build/hello_test[1]_tests.cmake")
-else()
-  add_test(hello_test_NOT_BUILT hello_test_NOT_BUILT)
-endif()
